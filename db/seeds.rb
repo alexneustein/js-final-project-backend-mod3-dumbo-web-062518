56e97a813f884be74c8607c014ca3ff4a4665bf6
runandrerun <the.asantiagojr@gmail.com>
2018-08-22 12:16:46 -0400

    Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
 
-User.create(username: "runandre")
-Channel.create(name: "COOL!")
-Message.create(user_id: 1, channel_id: 1, content: "WELCOME!!")
+User.destroy_all
+Channel.destroy_all
+Message.destroy_all
+
+10.times {
+  channel = Channel.create(name: Faker::GameOfThrones.house)
+}
+
+30.times {
+  user = User.create(username: Faker::GameOfThrones.character )
+}
+
+50.times {
+  message = Message.create(
+    content: Faker::GameOfThrones.quote,
+    user_id: Random.rand(40),
+    channel_id: Random.rand(40)
+  )
+}
+
+
+
+
+
+
+# User.create(username: "runandre")
+# Channel.create(name: "COOL!")
+# Message.create(user_id: 1, channel_id: 1, content: "WELCOME!!")
