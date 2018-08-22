c2b7610344e2bc6a7de423b2e673a6529f5944c0
runandrerun <the.asantiagojr@gmail.com>
2018-08-22 11:34:28 -0400

    cors

--- config/application.rb
+++ config/application.rb
@@ -32,13 +32,7 @@ module ChatApp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => :get,
-          :post, :delete, :put, :patch, :options, :head
-      end
-    end
+
 
   end
 end
