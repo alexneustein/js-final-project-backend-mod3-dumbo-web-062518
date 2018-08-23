2aad7cc38a73610836dde89b07e878949aac6a5f
Dan Chung <danetix@gmail.com>
2018-08-23 10:13:22 -0400

    new cable url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # config.action_cable.url = "ws://localhost:3000/cable"
-  config.action.cable.url = "ws://flatironchatterbox.herokuapp.com/cable"
+  config.action.cable.url = "wss://flatironchatterbox-api.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = ""
   config.action_cable.disable_request_forgery_protection = true
 end
