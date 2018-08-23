29ffbb18034c5b57cf89b4556a8e01f47c70eb37
Dan Chung <danetix@gmail.com>
2018-08-23 10:00:59 -0400

    changed development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.url = "ws://localhost:3000/cable"
+  # config.action_cable.url = "ws://localhost:3000/cable"
+  config.action.cable.url = "ws://flatironchatterbox.herokuapp.com/cable"
   # config.action_cable.allowed_request_origins = ""
   config.action_cable.disable_request_forgery_protection = true
 end
