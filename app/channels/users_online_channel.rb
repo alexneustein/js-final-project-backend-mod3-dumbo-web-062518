d30d172444e5045c60504c7deb75164bee083ec6
Dan Chung <danetix@gmail.com>
2018-08-24 11:30:52 -0400

    user joined notifications

--- app/channels/users_online_channel.rb
+++ app/channels/users_online_channel.rb
@@ -12,9 +12,9 @@ class UsersOnlineChannel < ApplicationCable::Channel
 
   def user_join(data)
     # puts data
-    User.create(username: data['username'], identifier: data['identifier'])
+    user = User.create(username: data['username'], identifier: data['identifier'])
     usernames = User.all_usernames
-    ActionCable.server.broadcast('users_online_channel', username: usernames)
+    ActionCable.server.broadcast('users_online_channel', username: usernames, new_user: user.username)
   end
 
   def user_leave(data)
