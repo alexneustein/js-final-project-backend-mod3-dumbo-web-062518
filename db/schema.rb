82c5b5b911a92de00642c7d29e160f033e840663
Dan Chung <danetix@gmail.com>
2018-08-24 12:24:59 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_08_24_152446) do
-=======
-ActiveRecord::Schema.define(version: 2018_08_24_135731) do
->>>>>>> d06377afff602ebb48ef45b3c798704e50ea3777
+ActiveRecord::Schema.define(version: 2018_08_24_160356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_08_24_135731) do
     t.integer "channel_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "effect"
     t.string "color"
   end
 
