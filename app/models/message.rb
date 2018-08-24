6152ec1562edde23c8005f87b2a743d0e3394014
Dan Chung <danetix@gmail.com>
2018-08-24 11:51:42 -0400

    validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,9 @@ class Message < ApplicationRecord
   # belongs_to :user
   # belongs_to :channel
 
+  validates :content, presence: true
+  validates :username, presence: true
+
   # after_create_commit do
   #   MessageCreationEventBroadcastJob.perform_later(self)
   # end
