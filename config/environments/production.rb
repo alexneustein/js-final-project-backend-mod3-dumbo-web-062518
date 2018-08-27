066283c1f04a085727e45e450ed853053630e8e8
Dan Chung <danetix@gmail.com>
2018-08-27 17:51:52 -0400

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action.cable.url = "wss://flatironchatterbox-api.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://flatironchatterbox.herokuapp.com', 'http://flatironchatterbox.herokuapp.com']
-  config.action_cable.disable_request_forgery_protection = true
+  # config.action.cable.url = "wss://flatironchatterbox-api.herokuapp.com/cable"
+  # config.action_cable.allowed_request_origins = ['https://flatironchatterbox.herokuapp.com', 'http://flatironchatterbox.herokuapp.com']
+  # config.action_cable.disable_request_forgery_protection = true
 end
